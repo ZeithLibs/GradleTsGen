6d150546525876036ffdc1504f3dbac16ee2505b
Zeith <me@zeith.org>
2026-05-01 03:53:59 +0300

    - 1.3.1 - Fix default values.

--- src/main/java/dev/zeith/jvmtsgen/util/GeneratorSettings.java
+++ src/main/java/dev/zeith/jvmtsgen/util/GeneratorSettings.java
@@ -5,14 +5,16 @@ import dev.zeith.tsgen.TSGenSettings;
 import lombok.*;
 import org.jetbrains.annotations.*;
 
+import java.util.Objects;
+
 @Setter
 @Getter
 public class GeneratorSettings
 {
 	@NotNull ImportStrategy importStrategy = ImportStrategy.IMPORT_FROM;
 	@Nullable Boolean enableVarargs;
-	@Nullable String newline;
-	@Nullable String indent;
+	@Nullable String newline = "\n";
+	@Nullable String indent = "\t";
 	boolean logSkippedClasses;
 	boolean detailedErrorLog;
 	boolean noTsCheck = true;
@@ -20,7 +22,13 @@ public class GeneratorSettings
 	public void apply(TSGenSettings.TSGenSettingsBuilder tsg)
 	{
 		if(enableVarargs != null) tsg.enableVarargs(enableVarargs);
-		if(newline != null) tsg.newline(newline);
-		if(indent != null) tsg.indent(indent);
+		if(isPresent(newline)) tsg.newline(newline);
+		if(isPresent(indent)) tsg.indent(indent);
+	}
+	
+	@Contract("null -> false; _ -> _")
+	private boolean isPresent(String s)
+	{
+		return !"null".equals(Objects.toString(s));
 	}
 }
